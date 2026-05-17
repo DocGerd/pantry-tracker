c5bb2298059eae37acc2584f2406c70656ede18c
Patrick Kuhn <live@docgerdsoft.de>
2026-05-17 08:53:03 +0200

    feat(m3): wire OffApiClient + Coil singleton; remove dev-seed scaffolding

--- app/src/main/java/de/docgerdsoft/pantrytracker/PantryTrackerApp.kt
+++ app/src/main/java/de/docgerdsoft/pantrytracker/PantryTrackerApp.kt
@@ -1,6 +1,9 @@
 package de.docgerdsoft.pantrytracker
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import de.docgerdsoft.pantrytracker.di.AppContainer
 
 class PantryTrackerApp : Application() {
@@ -10,5 +13,10 @@ class PantryTrackerApp : Application() {
     override fun onCreate() {
         super.onCreate()
         container = AppContainer(this)
+        SingletonImageLoader.setSafe { ctx ->
+            ImageLoader.Builder(ctx)
+                .components { add(OkHttpNetworkFetcherFactory()) }
+                .build()
+        }
     }
 }
