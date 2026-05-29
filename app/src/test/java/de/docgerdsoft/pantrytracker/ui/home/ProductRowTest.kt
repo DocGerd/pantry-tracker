f6fdf3b63a6cf3cbca423c4a115f1f8c0d798db8
Patrick Kuhn <live@docgerdsoft.de>
2026-05-29 11:20:41 +0200

    fix(test): fix import ordering in ProductRowTest to satisfy detekt (#190)

--- app/src/test/java/de/docgerdsoft/pantrytracker/ui/home/ProductRowTest.kt
+++ app/src/test/java/de/docgerdsoft/pantrytracker/ui/home/ProductRowTest.kt
@@ -5,12 +5,12 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import de.docgerdsoft.pantrytracker.data.local.Product
-import kotlin.time.Instant
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
+import kotlin.time.Instant
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [34])
